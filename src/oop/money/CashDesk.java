33512c1d0fdc57e5e51a38e75c26dc61e1bf9f5c
Rotstein007 <felix04.hofer@gmail.com>
2024-04-19 16:07:48 +0200

    temp

--- src/oop/money/CashDesk.java
+++ src/oop/money/CashDesk.java
@@ -1,9 +1,30 @@
 package oop.money;
 
+import java.sql.SQLOutput;
+import java.util.ArrayList;
+import java.util.List;
+
 public class CashDesk {
 
-    public static void pay() {
+    static double toPay = 34;
 
+    static double Payed = 50;
+
+    public static List<Double> pay() {
+
+        if (Payed < toPay) {
+            throw new IllegalArgumentException("Zu wenig bezahlt!");
+        }
+
+        double temp = 0;
+
+        temp = toPay - Payed;
+
+        System.out.println("Du hast " + Payed + "€ bezahlt bei einem Artikel Preis von " + toPay + "€. Du bekommst also " + temp + "€ zurück.");
+
+        List<Double> change = new ArrayList<>();
+
+        return change;
     }
 
 }
